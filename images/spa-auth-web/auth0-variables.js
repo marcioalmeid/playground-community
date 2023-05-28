f2c776f063aa98dabfdae600e5256d12c2801150
marcio.mendes <marcioalmeid@gmail.com>
2023-05-27 20:16:49 -0400

    Update auth0-variables.js

--- images/spa-auth-web/auth0-variables.js
+++ images/spa-auth-web/auth0-variables.js
@@ -1,5 +1,5 @@
 var AUTH0_CLIENT_ID='g7Lt3VKFsMxIPeDxuE4Yw7XeWLVzX8vF';
-var AUTH0_DOMAIN='manage.auth0.com';
-var AUTH0_AUDIENCE = 'https://dev-j2qyfg7ox7s2t54m.us.auth0.com/api/v2/';
+var AUTH0_DOMAIN='dev-j2qyfg7ox7s2t54m.us.auth0.com';
+var AUTH0_AUDIENCE = 'dev-j2qyfg7ox7s2t54m.us.auth0.com/api/v2/';
 var AUTH0_CALLBACK_URL = location.href;
 
